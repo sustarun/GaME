e7f2bfe957643d6063e289f32d89adb629f052a8
Aryan <aryanbdps9@gmail.com>
2018-11-02 16:58:00 +0530

    login_details

---
+++ GaME/login_details.py
@@ -0,0 +1,10 @@
+dbdict = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'game',
+        'USER': 'aryan',
+        'PASSWORD': 'your_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5053',
+    }
+}
