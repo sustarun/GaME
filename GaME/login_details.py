4e85b3025753dfe6cfaa8051cead9783be730a15
Aryan <aryanbdps9@gmail.com>
2018-11-02 16:45:39 +0530

    bug fix

--- GaME/login_details.py
+++ GaME/login_details.py
@@ -3,7 +3,7 @@ dbdict = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'game',
         'USER': 'aryan',
-        'PASSWORD': '',
+        'PASSWORD': 'your_password',
         'HOST': '127.0.0.1',
         'PORT': '5053',
     }
