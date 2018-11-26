8cfed19a919a6302b1b9dd348f5e459c854ea375
Aryan <aryanbdps9@gmail.com>
2018-11-26 19:18:50 +0530

    committed mistakes

--- GaME/login_details.py
+++ GaME/login_details.py
@@ -3,7 +3,7 @@ dbdict = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'game',
         'USER': 'aryan',
-        'PASSWORD': 'your_password',
+        'PASSWORD': '1Art-rot',
         'HOST': '127.0.0.1',
         'PORT': '5053',
     }
