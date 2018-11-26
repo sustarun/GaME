8c1140134fc394815387669053f22a565cc6da72
sustarun <sushanttarun13579@gmail.com>
2018-11-26 23:00:17 +0530

    Signup Done

--- GaME/login_details.py
+++ GaME/login_details.py
@@ -2,9 +2,9 @@ dbdict = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'game',
-        'USER': 'aryan',
+        'USER': 'tarun',
         'PASSWORD': 'your_password',
         'HOST': '127.0.0.1',
-        'PORT': '5053',
+        'PORT': '5550',
     }
 }
